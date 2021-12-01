48caa3019b4be44ada455ee3be6afdf492f4ca34
Jarquise Turner <j.turner348@gmail.com>
2021-12-01 00:27:25 -0500

    Update subcurrency.sol

--- subcurrency.sol
+++ subcurrency.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.10;
 
 // the contract should allow only the creator to create new coins
-
+// Anyone can send coins to each other without a need for registering with a username and password,all you need is an Ethereum keypair 
 
 contract Coin {
     
