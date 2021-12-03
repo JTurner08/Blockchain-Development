ef512945d3f48caa7e0d562cb2bee44ab3bc3fa1
Jarquise Turner <j.turner348@gmail.com>
2021-12-03 15:29:31 -0500

    Update voting.sol

--- solidity excercises/voting.sol
+++ solidity excercises/voting.sol
@@ -2,6 +2,9 @@ pragma solidity >=0.7.0 <0.9.0
 
 contract Ballot{
     // VARIABLES
+    strut vote {
+        address voterAddress
+    }
     struct voter {
         string voterName;
         bool voted;
