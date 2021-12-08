ca84073d2d96c06b03afac1773634dcbb47a6e83
Jarquise Turner <j.turner348@gmail.com>
2021-12-07 23:36:29 -0500

    Update _simple _Storage.sol

--- solidity excercises/_simple _Storage.sol
+++ solidity excercises/_simple _Storage.sol
@@ -2,7 +2,8 @@ Pragma solidity >=0.4.16 <0.9.0;
 
 contract SimpleStorage {
     uint storedData;
-    function set(uint x) {
-        
+   
+    function set(uint x) public {
+        storeData = x;
     }
 }
