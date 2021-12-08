aae5699b28efc9114716a8a41997d76f078f620c
Jarquise Turner <j.turner348@gmail.com>
2021-12-08 00:15:21 -0500

    Update whatAreEvents.sol

---
+++ solidity excercises/whatAreEvents.sol
@@ -0,0 +1,6 @@
+// Decentralized Exchange:  On a smart contract traders can trade tokens - from the smart contract emit an event so
+// they can get the data (web front moble)
+
+// After emitting events you can't read them in the past only for entities outside the of the blockchain - not stored as memory 
+// events have lower gas cost than  storage 
+
