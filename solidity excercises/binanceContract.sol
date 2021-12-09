728fbbf867dee761c3a44ed85c25efaeaa0d87ed
Jarquise Turner <j.turner348@gmail.com>
2021-12-09 13:02:34 -0500

    Update binanceContract.sol

--- solidity excercises/binanceContract.sol
+++ solidity excercises/binanceContract.sol
@@ -11,7 +11,7 @@ contract Token {
         balances[msg.sender] = totalSupply;
     }
 
-    function balanceOf(address owner) public view 
+    function balanceOf(address owner) public view returns(uint)
 }
 
 
