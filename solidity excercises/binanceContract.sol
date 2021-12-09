748f4aed457d7653bb3bf7f2ae759b9b9612ab4e
Jarquise Turner <j.turner348@gmail.com>
2021-12-09 12:58:49 -0500

    Update binanceContract.sol

--- solidity excercises/binanceContract.sol
+++ solidity excercises/binanceContract.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.2;
 
 contract Token {
+    mapping(address => uint) public balances;
     uint public totalSupply = 1000;
     string public name = "My Token";
     string public symbol = "TKN';
