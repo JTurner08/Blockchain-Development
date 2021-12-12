ead1b987f30c1ff3b6aa60e274d318eab5fe7c5a
Jarquise Turner <j.turner348@gmail.com>
2021-12-11 23:41:17 -0500

    Update binanceContract.sol

--- solidity excercises/binanceContract.sol
+++ solidity excercises/binanceContract.sol
@@ -32,7 +32,7 @@ function transferFrom(address from, address to, uint value) public returns(bool)
     require(allowance[from][msg.sender] >= value, 'allowance too low');
     balances[to] += value;
     balances[from] -= value;
-    emit Transer(msg.sender)
+    emit Transer(from,)
 }
 
 function approve(address spender, uint value) public returns(bool) {
