31835d35e4940736a4779947d0d65620175f867b
Jarquise Turner <j.turner348@gmail.com>
2022-01-02 23:01:00 -0500

    Update variables.sol

--- Blockchain Developer Bootcamp/variables.sol
+++ Blockchain Developer Bootcamp/variables.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.5.13;
 contract workingWithVariables {
     uint256 public myUint;
 
-    function setMyUint(uint _myUint) 
+    function setMyUint(uint _myUint) public
+        
 }
