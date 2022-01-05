a44dc6790d59c12ccccf370cd0d47ad96d9d4d62
Jarquise Turner <j.turner348@gmail.com>
2022-01-04 23:35:38 -0500

    Update covidTracker.sol

--- solidity excercises/covidTracker.sol
+++ solidity excercises/covidTracker.sol
@@ -1,5 +1,5 @@
 pragma solidty >=0.7.0 <0.9.0;
 
 library CyrptoSuite {
-    function splitSignature(bytes memory sig) internal pure returns(uint8 v, )
+    function splitSignature(bytes memory sig) internal pure returns(uint8 v, bytes32 r, bytes32 s)
 }
