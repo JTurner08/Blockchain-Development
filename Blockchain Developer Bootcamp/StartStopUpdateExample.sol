b268c577d5e6eec93c154d8b4b1e632d041655fc
Jarquise Turner <j.turner348@gmail.com>
2022-01-07 23:54:47 -0500

    Update StartStopUpdateExample.sol

--- Blockchain Developer Bootcamp/StartStopUpdateExample.sol
+++ Blockchain Developer Bootcamp/StartStopUpdateExample.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.5.13;
 
 contract StartStopUdateExample {
+
+    address ow/// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @return Documents the return variables of a contract’s function state variable
+    /// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
     function sendMoney() public payable {
 
     }
