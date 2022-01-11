6ecab7c68d893f5a0ea7bfb448f43c1b90e999db
Jarquise Turner <j.turner348@gmail.com>
2022-01-10 23:45:44 -0500

    Update StartStopUpdateExample.sol

--- Blockchain Developer Bootcamp/StartStopUpdateExample.sol
+++ Blockchain Developer Bootcamp/StartStopUpdateExample.sol
@@ -12,7 +12,7 @@ contract StartStopUdateExample {
     }
 
     function setPaused(bool _paused) public {
-        require
+        require(msg.)
     }
 
     function withdrawAllMoney(address payable _to) public {
