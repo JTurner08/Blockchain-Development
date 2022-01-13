ce0c4b0ba25c757b90728096910e2bfe1d8dce4a
Jarquise Turner <j.turner348@gmail.com>
2022-01-12 23:09:40 -0500

    Update StartStopUpdateExample.sol

--- Blockchain Developer Bootcamp/StartStopUpdateExample.sol
+++ Blockchain Developer Bootcamp/StartStopUpdateExample.sol
@@ -20,5 +20,5 @@ contract StartStopUdateExample {
         require(Paused== false,"Contract is paused");
         _to.transfer(address(this).balance)
         }
-        function 
+        function destroy
     }
