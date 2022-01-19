769925dff5efe2e74504ed380ef2f5cb01b98f94
Jarquise Turner <j.turner348@gmail.com>
2022-01-18 23:11:49 -0500

    Update Mapping.sol

--- Blockchain Developer Bootcamp/Mapping.sol
+++ Blockchain Developer Bootcamp/Mapping.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.6.0;
 
 contract simpleMappingExample {
+
     bool myVar1 = false;
     bool myVar2 = false;
     bool myvar3 = false;
+
     mapping(uint => bool) public myMapping;
 
     function setValue(uint _index) public{
     myMapping[_Index] = true;
     }
+
+    function 
 }
