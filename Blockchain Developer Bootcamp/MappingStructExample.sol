8c83ee1e519dfdab682b85a0a2af35a0f978e601
Jarquise Turner <j.turner348@gmail.com>
2022-01-27 23:54:57 -0500

    Update MappingStructExample.sol

--- Blockchain Developer Bootcamp/MappingStructExample.sol
+++ Blockchain Developer Bootcamp/MappingStructExample.sol
@@ -13,6 +13,6 @@ contract MappingStructExample {
     }
 
     function withdrawAllMoney(address payable _to) public {
-        uint balanceToSend = balance
+        uint balanceToSend = balanceReceived
     }
 }
