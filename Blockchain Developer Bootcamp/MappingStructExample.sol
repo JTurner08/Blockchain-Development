663a07ef99c2939d4fce69cc59385e5bcde3f630
Jarquise Turner <j.turner348@gmail.com>
2022-02-03 22:29:19 -0500

    Update MappingStructExample.sol

--- Blockchain Developer Bootcamp/MappingStructExample.sol
+++ Blockchain Developer Bootcamp/MappingStructExample.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.13;
 
 contract MappingStructExample {
-    mapping(address => uint) public balanceReceived;
+
+    struct
+    
+mapping(address => uint) public balanceReceived;
 
     function getbalance() public view returns(uint) {
         return address(this).balance;
