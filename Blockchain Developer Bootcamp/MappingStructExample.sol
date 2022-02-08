1e2bc92bb84875ca9981a8fc7f6f0012812583d1
Jarquise Turner <j.turner348@gmail.com>
2022-02-08 10:45:10 -0500

    Update MappingStructExample.sol

--- Blockchain Developer Bootcamp/MappingStructExample.sol
+++ Blockchain Developer Bootcamp/MappingStructExample.sol
@@ -26,7 +26,7 @@ mapping(address => uint) public balanceReceived;
 
     Payment memory payment = Payment(msg.value, now);
 
-    balanceReceived[msg.sender]
+    balanceReceived[msg.sender].payment
 
 
 function withdrawMoney(address payable _to, uint _amount) public {
