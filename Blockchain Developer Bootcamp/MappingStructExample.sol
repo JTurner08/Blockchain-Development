743b3cc394e4040ee2cbcdbfb209a55a3524b863
Jarquise Turner <j.turner348@gmail.com>
2022-02-10 16:21:15 -0500

    Update MappingStructExample.sol

--- Blockchain Developer Bootcamp/MappingStructExample.sol
+++ Blockchain Developer Bootcamp/MappingStructExample.sol
@@ -32,7 +32,7 @@ mapping(address => uint) public balanceReceived;
 
 function withdrawMoney(address payable _to, uint _amount) public {
     require(balanceReceived)[msg.sender].totalBalance >- _amount, "not enough funds");
-    balanceReceived[msg.sender]. -= _amount;
+    balanceReceived[msg.sender].total -= _amount;
     _to.transfer(_amount);
     
 }
