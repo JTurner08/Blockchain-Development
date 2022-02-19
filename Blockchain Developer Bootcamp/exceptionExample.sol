a76306459cd76c6df425451fe6a513171467aca8
Jarquise Turner <j.turner348@gmail.com>
2022-02-18 23:27:11 -0500

    Update exceptionExample.sol

--- Blockchain Developer Bootcamp/exceptionExample.sol
+++ Blockchain Developer Bootcamp/exceptionExample.sol
@@ -5,6 +5,6 @@ contract ExceptionExample {
 mapping(address => uint) public balanceReceived;
 
 function receivedMoney() public payable {
-    balanceReceived[msg.sender] + unit64(msg.value)
+    balanceReceived[msg.sender] +=
     }
 }
