e068590e29df95c0c767c7ea5c17eae9dd0e91e4
Jarquise Turner <j.turner348@gmail.com>
2022-02-19 00:37:23 -0500

    Update exceptionExample.sol

--- Blockchain Developer Bootcamp/exceptionExample.sol
+++ Blockchain Developer Bootcamp/exceptionExample.sol
@@ -8,5 +8,5 @@ function receivedMoney() public payable {
     balanceReceived[msg.sender] += msg.value;
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public
+    function withdrawMoney(address payable _to, uint _amount) public {}
 }
