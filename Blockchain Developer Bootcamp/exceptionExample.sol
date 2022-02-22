855ed02189d23f50f44e8e854a2193ddc7588cd3
Jarquise Turner <j.turner348@gmail.com>
2022-02-22 15:49:41 -0500

    Update exceptionExample.sol

--- Blockchain Developer Bootcamp/exceptionExample.sol
+++ Blockchain Developer Bootcamp/exceptionExample.sol
@@ -9,7 +9,7 @@ function receivedMoney() public payable {
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        require(_amount <= balanceReceived)[msg.sender],"you don't have enough et" -= _amount;
+        require(_amount <= balanceReceived)[msg.sender],"you don't have enough ether!" -= _amount;
         _to.transfer(_amount)
     }
 }
