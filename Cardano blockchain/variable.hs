ff2d9ef4bc5d3dc8e8c5e08c64706aee776914dc
Jarquise Turner <j.turner348@gmail.com>
2022-04-02 02:34:20 -0400

    Create variable.hs

---
+++ Cardano blockchain/variable.hs
@@ -0,0 +1,9 @@
+main = do
+    let name ="Dudley"
+
+    putStrLn(name ++ " received 36 gifts for his birthday.")
+    putStrLn("36 gifts? " ++ name ++ "thought out loud. But last year I had 37!")
+    putStrLn("This is when we knew.")
+    putStrLn(name ++ " was a very spoiled child.")
+
+    -- use hyphento comment out code in Haskell
