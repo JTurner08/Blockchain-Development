824a27df8105e06ab5c4a523460c913a79655fb8
Jarquise Turner <j.turner348@gmail.com>
2022-04-06 03:58:33 -0400

    Update variable.hs

--- Cardano blockchain/variable.hs
+++ Cardano blockchain/variable.hs
@@ -7,4 +7,4 @@ main = do
     putStrLn(name ++ " was a very spoiled child.")
 
     -- use hyphen to comment out code in Haskell
-    -- can make multiple comment using k
+    -- can make multiple comment using ke
