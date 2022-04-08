bad12ea08c4bdb7ade8078769e7cd38828c46a8b
Jarquise Turner <j.turner348@gmail.com>
2022-04-07 20:22:28 -1200

    Update variable.hs

--- Cardano blockchain/variable.hs
+++ Cardano blockchain/variable.hs
@@ -1,10 +1,12 @@
-main = do
-    let name ="Dudley"
 
-    putStrLn(name ++ " received 36 gifts for his birthday.")
-    putStrLn("36 gifts? " ++ name ++ "thought out loud. But last year I had 37!")
-    putStrLn("This is when we knew.")
-    putStrLn(name ++ " was a very spoiled child.")
+    name :: string
+    name ="Dudley"
+
+    numOfGifts :: Int
+    numOfGifts = 36
+
+    main = do
+        putStrLn name
 
     -- use hyphen to comment out code in Haskell
-    -- can make multiple comment using key 
+    -- can make multiple comment using key t
