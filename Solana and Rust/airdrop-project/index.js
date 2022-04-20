b56a24ac234b22a639cd247996e01f3ca537b2cf
Jarquise Turner <j.turner348@gmail.com>
2022-04-19 22:12:46 -1200

    Update index.js

--- Solana and Rust/airdrop-project/index.js
+++ Solana and Rust/airdrop-project/index.js
@@ -3,7 +3,7 @@ const {
     PublicKey,
     clusterApiUrl,
     Keypair,
-    LAMPORTS_PER_SOL
+    LAMPORTS_PER_SOL,
 } = require("@solana/web3.js")
  
 const wallet = new Keypair()
@@ -13,8 +13,14 @@ const secretKey = wallet._keypair.secretKey
  
 const getWalletBalance = async() => {
     try {
-
+        const Connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const WalletBalance = await Connection.getBalance(publicKey)
+        console.log('wallet balance is ${walletBalance}')
     }catch(err) {
         console.error(err)
     }
 }
+const main = async() => {
+    await getWalletBalance()
+}
+main()
