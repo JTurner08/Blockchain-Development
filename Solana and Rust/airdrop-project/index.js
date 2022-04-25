be42f85d02c9f33d3c234473baf06f264031064a
Jarquise Turner <j.turner348@gmail.com>
2022-04-24 23:38:50 -0400

    Update index.js

--- Solana and Rust/airdrop-project/index.js
+++ Solana and Rust/airdrop-project/index.js
@@ -8,7 +8,7 @@ const {
  
 const wallet = new Keypair()
  
-const publicKey = new PublicKey(wallet._keypair.public
+const publicKey = new PublicKey(wallet._keypair.publi
 const secretKey = wallet._keypair.secretKey
  
 const getWalletBalance = async() => {
